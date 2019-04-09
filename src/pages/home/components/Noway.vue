af8eb96a6789e823699d8cef0d9a44dcaee8d87a
Darki <125730892@qq.com>
2019-04-09 17:25:43 +0800

    save

--- src/pages/home/components/Noway.vue
+++ src/pages/home/components/Noway.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="noway">
-    
+
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      user: {
+        name: '顾客 0574',
+        
+      }
+    }
+  }
 }
 </script>
 
