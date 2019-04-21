19de090f30c82a7437caad6d01ce09b19c211bc7
Darki <125730892@qq.com>
2019-04-21 22:08:07 +0800

    mm

---
+++ src/pages/cart/components/CartList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cart-list">
+    <p>name</p>
+    <p>2磅</p>
+    <p>￥999</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus">
+.cart-list
+  margin-left 1.2rem
+  height 2rem
+  &~.cart-list
+    border-top 1px solid #eee
+</style>
--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="cart">
-
+    <cart-list/>
+    <cart-list/>
   </div>
 </template>
 
 <script>
+import CartList from './components/CartList'
 export default {
-
+  components: {
+    CartList
+  }
 }
 </script>
 
 <style lang="stylus">
-
+.cart
+  height 100%
+  background-color #f8f8f8
 </style>
